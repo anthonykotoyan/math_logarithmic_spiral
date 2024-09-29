bf34f0f00254e075e24164b25b265d27beb67b3f
akotoy <148432161+anthonykotoyan@users.noreply.github.com>
2024-09-28 21:43:36 -0700

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -24,6 +24,14 @@ class Vector2:
         return endPos
 
     @staticmethod
+    def addVectors(vec1, vec2):
+
+        x = vec1.x+vec2.x
+        y = vec1.y + vec2.y
+
+        return Vector2(math.sqrt(x ** 2 + y ** 2), math.atan2(y, x))
+
+    @staticmethod
     def avgVector(listOfVectors):
         if not listOfVectors:
             return Vector2(0,0)
@@ -31,14 +39,20 @@ class Vector2:
         cumulativeX = 0
         cumulativeY = 0
         totalVectors = len(listOfVectors)
+
+        # Iterate through the list and sum up the vectors incrementally
         for i in range(totalVectors):
             cumulativeX += listOfVectors[i].x
             cumulativeY += listOfVectors[i].y
             for j in range(i):
                 cumulativeX += listOfVectors[j].x
                 cumulativeY += listOfVectors[j].y
+
+        # Divide by the total number of vectors
         avgX = cumulativeX / totalVectors
         avgY = cumulativeY / totalVectors
+
+        # Convert back to polar coordinates (r, theta)
         avgR = math.sqrt(avgX ** 2 + avgY ** 2)
         avgTheta = math.atan2(avgY, avgX)
 
